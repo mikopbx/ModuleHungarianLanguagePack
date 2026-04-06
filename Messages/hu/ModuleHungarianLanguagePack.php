59722e86a445f50542e7e86ac55f21644300bb47
Nikolay Beketov <nbek@miko.ru>
2026-04-06 14:21:48 +0700

    Initial commit: ModuleHungarianLanguagePack language pack for MikoPBX

---
+++ Messages/hu/ModuleHungarianLanguagePack.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'BreadcrumbModuleHungarianLanguagePack' => 'Magyar',
+    'SubHeaderModuleHungarianLanguagePack' => 'Magyar - MikoPBX',
+];
---
+++ Messages/ru/ModuleHungarianLanguagePack.php
@@ -0,0 +1,8 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'BreadcrumbModuleHungarianLanguagePack' => 'Языковой пакет - Венгерский',
+    'SubHeaderModuleHungarianLanguagePack' => 'Комплексная поддержка венгерского языка для системы MikoPBX',
+];
